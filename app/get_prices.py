b29f8b96c89c3cb02b3224b5129e3f1ebb1b85db
Cryxelia <luhuan.olsson@gmail.com>
2025-01-18 21:04:44 +0100

    small changes to everything

--- app/get_prices.py
+++ app/get_prices.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 options = webdriver.ChromeOptions()
 
 options.add_argument("--headless=new")
@@ -13,7 +14,9 @@ driver.get("https://teijasskateshop.com/konstakningsskridskor/edea/")
 
 products = driver.find_elements(By.CSS_SELECTOR, ".product-item")
 
+
 extracted_products = []
+
 
 for product in products:
 
@@ -21,12 +24,12 @@ for product in products:
         "name": product.find_element(By.CSS_SELECTOR, ".product-item__heading").text,
         "price": product.find_element(By.CSS_SELECTOR, ".price").text,
     }
-
     
     extracted_products.append(product_data)
 
-finished_list = extracted_products[:-3]
 
+finished_list = extracted_products[:-3]
 print(finished_list)
+
 
 driver.quit()
