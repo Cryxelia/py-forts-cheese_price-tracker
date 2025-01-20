7578f3a9aa38f0ec034f897adfa4e9ee4a08dd1d
Cryxelia <luhuan.olsson@gmail.com>
2025-01-20 10:32:37 +0100

    moving code tothe main program

--- app/get_prices.py
+++ app/get_prices.py
@@ -1,34 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
 
-options = webdriver.ChromeOptions()
-
-options.add_argument("--headless=new")
-
-driver = webdriver.Chrome(
-    options=options,
+chrome_options = Options()
+chrome_options.add_argument("--headless") 
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.265 Safari/537.36"
 )
 
-driver.get("https://teijasskateshop.com/konstakningsskridskor/edea/")
+service = Service(r"C:\Users\Freja Olsson\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe") 
 
-products = driver.find_elements(By.CSS_SELECTOR, ".product-item")
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
+url = "https://www.coop.se/handla/varor/ost/?brand=Arla%C2%AE"
+driver.get(url)
 
-extracted_products = []
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "ProductTeaser"))
+)
 
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
 
-for product in products:
+cheese_section = soup.find_all('div', 'ProductTeaser-content')
 
-    product_data = {
-        "name": product.find_element(By.CSS_SELECTOR, ".product-item__heading").text,
-        "price": product.find_element(By.CSS_SELECTOR, ".price").text,
-    }
-    
-    extracted_products.append(product_data)
-
-
-finished_list = extracted_products[:-3]
-print(finished_list)
-
+print("all cheese")
+for i, ost in enumerate(cheese_section[:10], start=1):  
+    print(f"{i}. {ost.text.strip()}")
 
 driver.quit()
